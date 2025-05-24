d9ce2a79e68d5ae7214c5e9746786c4bf058b981
danny <xi4869@gmail.com>
2025-05-24 15:06:42 +0800

    fix: 調整 HomePage 組件的導入順序，簡化 Grid 組件的使用，並優化 CardActions 的樣式

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
-import { Container, Grid, CardHeader, Card, CardActions, CardContent } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
+
+import { Card, CardActions, CardContent, CardHeader, Container, Grid } from '@mui/material';
 import Button from '@mui/material/Button';
 
 const HomePage = () => {
@@ -8,24 +9,24 @@ const HomePage = () => {
       <Card>
         <CardHeader title="日文練習" sx={{ textAlign: 'center' }}></CardHeader>
         <CardContent color="primary"></CardContent>
-        <CardActions>
-          <Grid container justifyContent="center" alignItems="center" spacing={6}>
-            <Grid item xs={12} sm={6}>
+        <CardActions sx={{ justifyContent: 'center' }}>
+          <Grid container spacing={12}>
+            <Grid size={{ xs: 12, sm: 6 }}>
               <Button sx={{ width: '100%' }} component={RouterLink} to="/new" variant="contained" size="large">
                 新增
               </Button>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid size={{ xs: 12, sm: 6 }}>
               <Button sx={{ width: '100%' }} component={RouterLink} to="/quiz" variant="contained" size="large">
                 測驗
               </Button>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid size={{ xs: 12, sm: 6 }}>
               <Button sx={{ width: '100%' }} component={RouterLink} to="/study" variant="contained" size="large">
                 閱讀
               </Button>
             </Grid>
-            <Grid item xs={12} sm={6}>
+            <Grid size={{ xs: 12, sm: 6 }}>
               <Button sx={{ width: '100%' }} component={RouterLink} to="/about" variant="contained" size="large">
                 關於
               </Button>
