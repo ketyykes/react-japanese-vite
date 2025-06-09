18ddee585fcba8fe8cb067543691ab86c321e51f
danny <xi4869@gmail.com>
2025-06-09 21:45:03 +0800

    feat: 更新 Vite 配置，新增 base 設定為根目錄，改善專案的部署與路徑解析

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
